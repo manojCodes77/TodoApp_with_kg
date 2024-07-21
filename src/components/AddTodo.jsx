8d01796a63d213bdf8cc180441c42ced9033734d
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-21 18:04:53 +0530

    creating_form

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -12,27 +12,30 @@ function AddTodo({onNewItem}) {
         setTodoDate(e.target.value);
         // console.log(todoDate);
     }
-    const handleAddButtonClicked=()=>{
+    const handleAddButtonClicked=(event)=>{
+        event.preventDefault();
         onNewItem(todoName,todoDate);
         setTodoDate("");
         setTodoName("");
     }
     return (
-        <div className="grid grid-cols-3 gap-4 mb-4 bg-slate-700 p-2 rounded-lg">
+        <form
+        onSubmit={handleAddButtonClicked}
+        className="grid grid-cols-3 gap-4 mb-4 bg-slate-700 p-2 rounded-lg">
             <div className="text-black flex items-center justify-center">
-                <input type="text" className="p-1 rounded-md" placeholder="Add Task" value={todoName} onChange={handleNameChange} />
+                <input type="text" className="p-1 rounded-md" placeholder="Add Task" value={todoName} onChange={handleNameChange} required />
             </div>
             <div className="text-black flex items-center justify-center">
-                <input type="date" className="p-1 rounded-md" value={todoDate} onChange={handleDateChange} />
+                <input type="date" className="p-1 rounded-md" value={todoDate} onChange={handleDateChange} required />
             </div>
             <div className="flex items-center justify-center">
-                <button type="button"
-                    onClick={handleAddButtonClicked}
+                <button
+                // onClick={handleAddButtonClicked}
                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
                     <IoIosAddCircle />
                 </button>
             </div>
-        </div>
+        </form>
     );
 }
 export default AddTodo;
