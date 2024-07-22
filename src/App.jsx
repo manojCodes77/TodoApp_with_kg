0045fb7cd28911a2ca2bd164307b7809e12a6996
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-22 21:48:13 +0530

    completely seperating business logic from UI or App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,12 @@ import AppName from './components/AddName.jsx'
 import AddTodo from './components/AddTodo.jsx'
 import TodoItems from './components/TodoItems.jsx'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import { TodoItemsContext } from './store/todo-items-store.jsx'
-import { useReducer, useState } from 'react'
-
-const reducer = (currTodoItems, action) => {
-  let newTodoItems = currTodoItems;
-  if (action.type === 'ADD') {
-    newTodoItems= [...currTodoItems, action.payload];
-  } else if (action.type === 'DELETE') {
-    newTodoItems= currTodoItems.filter((item, i) => i !== action.payload);
-  }
-  return newTodoItems;
-}
+import TodoItemsContextProvider from './store/todo-items-store.jsx'
 
 function App() {
-  const [todoItems, dispatch] = useReducer(reducer, []);
-  const handleNewItem = (itemName, itemDueDate) => {
-    console.log(`new item added ${itemName} due date ${itemDueDate}`);
-    dispatch({ type: 'ADD', payload: { todoName: itemName, todoDate: itemDueDate } });
-  }
-
-  const handleDeleteItem = (index) => {
-    console.log(`item deleted at index ${index}`);
-    dispatch({ type: 'DELETE', payload: index });
-  }
-
   return (
     <>
-      <TodoItemsContext.Provider value={
-        {
-          todoItems: todoItems,
-          handleNewItem: handleNewItem,
-          handleDeleteItem: handleDeleteItem
-        }
-      }>
+      <TodoItemsContextProvider>
         <div className="BigContainer bg-red-800 text-white min-h-screen p-9">
           <center className="todo-container" >
             <AppName />
@@ -44,7 +16,7 @@ function App() {
             <TodoItems />
           </center>
         </div>
-      </TodoItemsContext.Provider>
+      </TodoItemsContextProvider>
     </>
   )
 }
--- src/store/todo-items-store.jsx
+++ src/store/todo-items-store.jsx
@@ -1,9 +1,45 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
-export const TodoItemsContext=createContext(
+export const TodoItemsContext = createContext(
     {
-        todoItems:[],
-        handleNewItem:()=>{},
-        handleDeleteItem:()=>{}
+        todoItems: [],
+        handleNewItem: () => { },
+        handleDeleteItem: () => { }
     }
 );
+const reducer = (currTodoItems, action) => {
+    let newTodoItems = currTodoItems;
+    if (action.type === 'ADD') {
+        newTodoItems = [...currTodoItems, action.payload];
+    } else if (action.type === 'DELETE') {
+        newTodoItems = currTodoItems.filter((item, i) => i !== action.payload);
+    }
+    return newTodoItems;
+}
+const TodoItemsContextProvider = ({ children }) => {
+
+    const [todoItems, dispatch] = useReducer(reducer, []);
+    const handleNewItem = (itemName, itemDueDate) => {
+        console.log(`new item added ${itemName} due date ${itemDueDate}`);
+        dispatch({ type: 'ADD', payload: { todoName: itemName, todoDate: itemDueDate } });
+    }
+
+    const handleDeleteItem = (index) => {
+        console.log(`item deleted at index ${index}`);
+        dispatch({ type: 'DELETE', payload: index });
+    }
+    return (
+        <TodoItemsContext.Provider value={
+            {
+                todoItems: todoItems,
+                handleNewItem: handleNewItem,
+                handleDeleteItem: handleDeleteItem
+            }
+        }>
+            {children}
+        </TodoItemsContext.Provider>
+    );
+}
+
+export default TodoItemsContextProvider;
